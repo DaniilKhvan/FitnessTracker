7a61d2b494b8c6203832b3f4aa54d9dec38b8e86
DaniilKhvan <240656@astanait.edu.kz>
2024-12-30 10:45:46 +0500

    Update FitnessApp.java

--- FitnessApp.java
+++ FitnessApp.java
@@ -1,28 +1,45 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FitnessApp {
-    private User user;
-    private List<WorkoutRoutine> workoutRoutines;
+    private List<WorkoutRoutine> routines;
 
-    public FitnessApp(User user) {
-        this.user = user;
-        this.workoutRoutines = new ArrayList<>();
+    public FitnessApp() {
+        this.routines = new ArrayList<>();
     }
 
-    public void addWorkoutRoutine(WorkoutRoutine workout) {
-        workoutRoutines.add(workout);
+    public void addRoutine(WorkoutRoutine routine) {
+        routines.add(routine);
     }
 
-    public void displayWorkoutRoutines() {
-        System.out.println("Workout Routines for " + user.getName() + ":");
-        for (WorkoutRoutine routine : workoutRoutines) {
-            System.out.println(routine);
+    public List<WorkoutRoutine> searchByName(String name) {
+        return routines.stream()
+                       .filter(r -> r.getName().equalsIgnoreCase(name))
+                       .collect(Collectors.toList());
+    }
+
+    public List<WorkoutRoutine> filterByCalories(int minCalories, int maxCalories) {
+        return routines.stream()
+                       .filter(r -> {
+                           int calories = r.calculateCaloriesBurned();
+                           return calories >= minCalories && calories <= maxCalories;
+                       })
+                       .collect(Collectors.toList());
+    }
+
+    public List<WorkoutRoutine> sortByDate() {
+        return routines.stream()
+                       .sorted(Comparator.comparing(WorkoutRoutine::getDate))
+                       .collect(Collectors.toList());
+    }
+
+    public void printAllRoutines() {
+        if (routines.isEmpty()) {
+            System.out.println("No routines added yet!");
+        } else {
+            routines.forEach(System.out::println);
         }
-    }
-
-    @Override
-    public String toString() {
-        return "FitnessApp{user=" + user + ", workoutRoutines=" + workoutRoutines + "}";
     }
 }
